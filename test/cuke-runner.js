07342357658c06da902b1708c66933cec6ac8486
Vincent Barbin <git@vincentbarbin.net>
2014-03-26 09:13:42 -0400

    Changed filtering of cuke files

--- test/cuke-runner.js
+++ test/cuke-runner.js
@@ -1,12 +1,10 @@
 var cuke = require('../lib');
 var fs = require('fs');
 var path = require('path');
-var ls = fs.readdirSync(__dirname).map(function(itemName) {
-    if (/\.cuke/.test(itemName)) {
-        return /(.*?)\.cuke/.exec(itemName)[1];
-    }
-}).filter(function(item) {
-    return !!item;
+var ls = fs.readdirSync(__dirname).filter(function(item) {
+    return /\.cuke/.test(item);
+}).map(function(itemName) {
+    return /(.*?)\.cuke/.exec(itemName)[1];
 });
 
 for (var i = 0; i < ls.length; i++) {
